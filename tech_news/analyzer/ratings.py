29144753d6f568772e7b481cab89a098c38c99d9
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2023-01-24 13:45:31 -0300

    req 11

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,5 @@
 from tech_news.database import find_news
+from collections import Counter
 
 
 # Requisito 10
@@ -9,7 +10,7 @@ def top_5_news():
         news, key=lambda x: (x["comments_count"], x["title"]), reverse=True
     )
 
-    five_news = [(new['title'], new['url']) for new in sorted_news[:5]]
+    five_news = [(new["title"], new["url"]) for new in sorted_news[:5]]
 
     return five_news
 
@@ -17,6 +18,10 @@ def top_5_news():
 # Requisito 11
 def top_5_categories():
     """Seu código deve vir aqui"""
+    news = find_news()
+    categories = [new["category"] for new in news]
+    sorted_categories = sorted(categories)
+    categories_count = Counter(sorted_categories).most_common()
+    topFive = [category[0] for category in categories_count[:5]]
 
-
-top_5_news()
+    return topFive
